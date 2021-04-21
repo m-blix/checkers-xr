875dff54ef8e85964f1a67d2c3c245f186eebf2d
Michael Blix <m.blix@samsung.com>
2021-04-21 03:39:01 +0000

    Init piece interaction

--- js/app.js
+++ js/app.js
@@ -1,21 +1,41 @@
 'use strict';
 
-let $ = document.getElementById.bind(document);
+let $ = document.querySelector.bind(document);
 
-let sceneEl = document.getElementById('scene');
-let boardEl = document.getElementById('board');
+let sceneEl = $('#scene');
+let boardEl = $('#board');
+
+let N = 8;
+let size = 8;
+
+let els = [];
+let darkSquares = [];
 
 function setup() {
   console.log('app setup');
-  createBoard();
+
+  createBoard(N, size);
+  createPieces(N, size);
+
+  let piece = $('#board');
+  piece.addEventListener('click', function(e){
+    console.log('click piece');
+  });
+  piece.addEventListener('mouseenter', function(e) {
+    console.log('start hover');
+    e.target.dataset.ogColor = e.target.getAttribute('color');
+    e.target.setAttribute('material','color','#cf8888');
+  });
+  piece.addEventListener('mouseleave', function(e) {
+    console.log('end hover');
+    e.target.setAttribute('material','color', e.target.dataset.ogColor);
+  });
 }
 
-function createBoard() {
+function createBoard(N = 8, size = 8) {
   const colorA = '#389651';
   const colorB = '#ddd';
 
-  const N = 8;
-  const size = 8;
   let offset = size/2 - 0.5; //
 
   for (let i = 0; i < N; i++) {
@@ -28,8 +48,15 @@ function createBoard() {
       el.setAttribute('color', color);
       el.setAttribute('height', size/N);
       el.setAttribute('width', size/N);
-      el.setAttribute('position', {x: i - offset, y: j - offset, z: 0});
+      el.object3D.position.set(i - offset, j - offset, 0);
+      el.dataset.row = i;
+      el.dataset.col = j;
       boardEl.appendChild(el);
+
+      els.push(el);
+      if (!((i%2 && j%2) || (i%2==0 && j%2==0))) {
+        darkSquares.push(el);
+      }
     }
   }
 
@@ -41,6 +68,41 @@ function createBoard() {
   borderEl.setAttribute('depth', boardH);
   borderEl.setAttribute('position', {x: 0, y: 0, z: -boardH*0.51 });
   boardEl.appendChild(borderEl);
+}
+
+function createPieces(N = 8, size = 8) {
+  const numPiecesPerPlayer = 12;
+
+
+  for (let i = 0; i < numPiecesPerPlayer; i++) {
+    let piece = document.createElement('a-cylinder');
+    piece.setAttribute('radius', 0.4);
+    piece.setAttribute('height', 0.2);
+    piece.setAttribute('color', "#c90e0e");
+    piece.setAttribute('rotation', '-90 0 0');
+    piece.classList.add('raycastable');
+
+    let posX = darkSquares[i].getAttribute('position').x;
+    let posY = darkSquares[i].getAttribute('position').y;
+
+    piece.object3D.position.set(posX, posY, 0);
+    boardEl.appendChild(piece);
+  }
+
+  for (let i = 0; i < numPiecesPerPlayer; i++) {
+    let piece = document.createElement('a-cylinder');
+    piece.setAttribute('radius', 0.4);
+    piece.setAttribute('height', 0.2);
+    piece.setAttribute('color', "#333");
+    piece.setAttribute('rotation', '-90 0 0');
+    piece.classList.add('raycastable');
+
+    let posX = darkSquares[i+20].getAttribute('position').x;
+    let posY = darkSquares[i+20].getAttribute('position').y;
+
+    piece.object3D.position.set(posX, posY, 0);
+    boardEl.appendChild(piece);
+  }
 }
 
 setup();
